803c7e1e324539e21f7bcf757fb53c577f001add
zidano88 <s-omarzidan@zewailcity.edu.eg>
2023-07-03 03:19:46 +0300

    first commit

---
+++ config/plugins.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: env("SENDGRID_DEFAULT_FROM"),
+        defaultReplyTo: env("SENDGRID_DEFAULT_TO"),
+      },
+    },
+  },
+  // ...
+});
